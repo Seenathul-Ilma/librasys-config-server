f2904288bc629f8540dfc5984aa0cb70e09c4db7
ZeenathulIlma <seenathulilma121243@gmail.com>
2026-08-23 07:59:28 +0530

    Initial commit: config-server

---
+++ src/test/java/lk/ijse/eca/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.eca.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
